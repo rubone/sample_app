d2f75ca38295f7c267b2a03539111629f1c4bbea
rubone <rubone02@gmail.com>
2014-01-02 23:06:45 +0000

    Make a basic User model (including secure passwords)

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
   subject { page }
   
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
-    #it { should have_title(full_title(page_title)) }
+    it { should have_title(full_title(page_title)) }
   end 
 
   describe "Home page" do
    before {visit root_path} 
-   let {:heading}    {'Sample app'}
-   let {:page_title} { '' } 
+   let (:heading)    { 'Sample app' }
+   let (:page_title) { '' }
+    
    it_should_behave_like "all static pages"
    it { should_not have_title('| Home') }
    
-   #it { should have_content('Sample App') }
-   #it { should have_title('') }
-   #it { should_not have_title('| Home') }  
   end
   
   describe "Help page" do
@@ -27,8 +24,8 @@ describe "Static pages" do
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| Help') }
-     it { should have_selector('h1', text: 'Help') }
+    #it { should_not have_title('| Help') }
+    #it { should have_selector('h1', text: 'Help') }
     #it { should have_content('Help') }
     #it { should have_title(full_title('Help')) }
   end
@@ -39,8 +36,8 @@ describe "Static pages" do
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| About') }
-     it { should have_selector('h1', text: 'About') }
+    #it { should_not have_title('| About') }
+    #it { should have_selector('h1', text: 'About') }
     #it { should have_content('About') }
     #it { should have_title(full_title('About Us')) }    
   end
@@ -51,26 +48,26 @@ describe "Static pages" do
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
-    it { should_not have_title('| Contact') }
+    #it { should_not have_title('| Contact') }
     
     #it { should have_content('Contact') }
-    it { should have_selector('h1', text: 'Contact') }
+    #it { should have_selector('h1', text: 'Contact') }
     #it { should have_title(full_title('Contact')) }
   end
   
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    expect(page).to have_title(full_title('About Us'))
+    expect(page).to have_title(full_title('About'))
     click_link "Help"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Help'))
     click_link "Contact"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to # fill in
+    expect(page).to #have_title(full_title('Sign up now!'))
     click_link "sample app"
-    expect(page).to # fill in
+    expect(page).to #have_title(full_title('Home'))
   end
 end
 
